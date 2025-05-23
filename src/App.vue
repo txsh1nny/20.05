db7db350e1ddbf6ce28a07d8de3fc2a1c2acf1ac
Administrator <user@gmail.com>
2025-05-23 09:28:35 +0500

    установил модули пениа date-nfc бутсрап

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-cloud">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">NoteCloud</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navMenu">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><router-link class="nav-link" to="/notes">All Notes</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/add">New Note</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/about">About</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <transition name="fade" mode="out-in">
+      <router-view class="container my-4" />
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount } from 'vue'
+import { useNoteStore }  from './stores/noteStore'
+
+const store = useNoteStore()
+onBeforeMount(() => {
+  store.loadNotes()
+})
+</script>
